b464a13432677f05a71654967d4cf170306e53e4
axel <axel@epitech.eu>
2026-05-03 17:35:09 +0400

    V3

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Axel-Arnold-Portfolio-v2/",
+  base: "/", // ✔️ obligatoire
   plugins: [react()],
 });
